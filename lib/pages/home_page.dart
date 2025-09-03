d96e34cbeb0868f64e629cc4af1a9442bd72a7fa
Dhruvvv <dhruvpanchal547@gmail.com>
2025-09-03 11:25:18 +0530

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../data.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  var _deviceHeight;
+  var _deviceWidth;
+
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    // TODO: implement build
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[_featuredGamesWidget()],
+      ),
+    );
+  }
+
+  Widget _featuredGamesWidget() {
+    return SizedBox(
+      height: _deviceHeight * 0.50,
+      width: _deviceWidth,
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(featuredGames[0].coverImage.url))),
+      ),
+    );
+  }
+}
