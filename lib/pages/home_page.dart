585c9c53b32e5bb37b8e4f243eba28d8f5c65411
Dhruvvv <dhruvpanchal547@gmail.com>
2025-09-13 22:52:59 +0530

    2nd Slider Games Window Added(P1)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../data.dart';
-
+import '../widgets/scrollable_games_widget.dart';
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -11,9 +12,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var _deviceHeight;
   var _deviceWidth;
+  var _selectedGame;
 
   void initState() {
     super.initState();
+    _selectedGame=0;
   }
 
   @override
@@ -23,25 +26,135 @@ class _HomePageState extends State<HomePage> {
     // TODO: implement build
     return Scaffold(
       body: Stack(
-        children: <Widget>[_featuredGamesWidget()],
+        children: <Widget>[
+          _featuredGamesWidget(),
+          _gradientBoxWidget(),
+          _topLayerWidget(),
+        ],
       ),
     );
   }
 
   Widget _featuredGamesWidget() {
     return SizedBox(
-      height: _deviceHeight * 0.50,
-      width: _deviceWidth,
-      child: PageView(
-        scrollDirection: Axis.horizontal,
-        children: featuredGames.map((_game){
-        return Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
+        height: _deviceHeight * 0.50,
+        width: _deviceWidth,
+        child: PageView(
+          onPageChanged:(_index){
+              setState(() {
+                _selectedGame=_index;
+              });
+          },
+          scrollDirection: Axis.horizontal,
+          children: featuredGames.map((_game) {
+            return Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
                   fit: BoxFit.cover,
-                  image: NetworkImage(_game.coverImage.url),),),
-        );
-      }).toList(),)
-    );
+                  image: NetworkImage(_game.coverImage.url),
+                ),
+              ),
+            );
+          }).toList(),
+        ));
   }
+
+  Widget _gradientBoxWidget() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+        child: Container(
+      height: _deviceHeight * 0.80,
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(35, 45, 59, 1.0),
+            Colors.transparent,
+          ],
+          stops: [0.65, 1.0],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
+    ));
+  }
+  Widget _topLayerWidget(){
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth*0.05,vertical: _deviceHeight*0.005),
+      child:Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        _topBarWidget(),
+        SizedBox(height: _deviceHeight*0.13),
+        _featuredGamesInfoWidget(),
+        ScrollableGamesWidget(_deviceHeight*0.24, _deviceWidth, true, games),
+      ],
+    ),);
+  }
+  Widget _topBarWidget(){
+    return SizedBox(height: _deviceHeight *0.12,
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,mainAxisSize: MainAxisSize.max,crossAxisAlignment: CrossAxisAlignment.center,children: <Widget>[
+        Icon(
+          Icons.menu,
+          color: Colors.white,
+          size: 30,
+        ),
+        Row(children: <Widget>[
+          Icon(
+          Icons.search,
+          color: Colors.white,
+          size: 30,
+        ),
+          SizedBox(
+            width: _deviceWidth*0.03,
+          ),
+          Icon(
+            Icons.notifications_none,
+            color: Colors.white,
+            size: 30,
+          ),],)
+      ],),);}
+    Widget _featuredGamesInfoWidget(){
+      return SizedBox(
+          height: _deviceHeight*0.12,
+          width: _deviceWidth,
+          child:Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                featuredGames[_selectedGame].title,
+                 maxLines: 2,
+                 style:
+                 TextStyle(color: Colors.white,fontSize: _deviceHeight*0.040),
+              ),
+            //  SizedBox(height: _deviceHeight*0.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+
+                children: featuredGames.map((_game){
+                  bool _isActive=_game.title==featuredGames[_selectedGame].title;
+                  double _circleRadius=_deviceHeight*0.003;
+                  return Container(
+                    margin: EdgeInsets.only(right: _deviceHeight*0.015),
+                    height: _circleRadius*2,
+                  width: _circleRadius*2,
+                    decoration: BoxDecoration(
+                        color:_isActive ? Colors.green:Colors.grey,
+                        borderRadius: BorderRadius.circular(100)
+                    ),
+                  );
+                }).toList(),
+              )
+            ],
+          )
+      );
+    }
+
+
 }
